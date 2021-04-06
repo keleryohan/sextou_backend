c51bef0ef258bf08c889b95676ec6ac67aba2739
Levir Chianca <levirchianca@MacBook-Air-de-Levir.local>
2021-04-06 18:38:49 -0300

    Feat: Configurações básicas

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,41 @@
-import "reflect-metadata";
-import express from 'express';
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import '@infra/typeorm';
+import '@container/index';
+
+import AppError from '@errors/AppError';
 
 import routes from './routes';
 
 const app = express();
 
+const port = process.env.PORT || 3333;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('common'));
 app.use(routes);
 
-app.listen('3333', () => console.log("Subiu"));
+// Global error handling
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    message: 'Error inesperado dentro do servidor'
+  });
+});
+
+
+app.listen(port, () => console.log(`🖌  server started on port ${port}`));
--- src/infra/typeorm/index.ts
+++ src/infra/typeorm/index.ts
@@ -1,3 +1,3 @@
 import { createConnections } from 'typeorm';
 
-createConnections();
+createConnections().then(() => console.log('🎁 Sucessfully connected with database'));
